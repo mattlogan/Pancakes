be86f2b40b8aac9a436c306a0e702accc6b2a2c6
Matt Logan <matt.b.logan@gmail.com>
2015-09-25 22:11:16 -0600

    Added Preconditions for null checks

---
+++ library/src/main/java/me/mattlogan/library/Preconditions.java
@@ -0,0 +1,14 @@
+package me.mattlogan.library;
+
+final class Preconditions {
+
+    static void checkNotNull(Object o, String message) {
+        if (o == null) {
+            throw new NullPointerException(message);
+        }
+    }
+
+    private Preconditions() {
+        throw new AssertionError("No instances");
+    }
+}
--- library/src/main/java/me/mattlogan/library/ViewStack.java
+++ library/src/main/java/me/mattlogan/library/ViewStack.java
@@ -5,6 +5,8 @@ import android.view.ViewGroup;
 
 import java.util.Stack;
 
+import static me.mattlogan.library.Preconditions.checkNotNull;
+
 public final class ViewStack {
 
     private static final String STACK_TAG = "stack";
@@ -14,6 +16,8 @@ public final class ViewStack {
     private final ViewStackDelegate delegate;
 
     public static ViewStack create(ViewGroup container, ViewStackDelegate delegate) {
+        checkNotNull(container, "container == null");
+        checkNotNull(delegate, "delegate == null");
         return new ViewStack(container, delegate);
     }
 
@@ -23,14 +27,16 @@ public final class ViewStack {
     }
 
     public ViewFactory push(ViewFactory viewFactory) {
+        checkNotNull(viewFactory, "viewFactory == null");
         stack.push(viewFactory);
         updateContainer();
         return viewFactory;
     }
 
-    @SuppressWarnings("all")
     public void rebuildFromBundle(Bundle bundle) {
+        @SuppressWarnings("unchecked")
         Stack<ViewFactory> savedStack = (Stack<ViewFactory>) bundle.getSerializable(STACK_TAG);
+        checkNotNull(savedStack, "Bundle doesn't contain any ViewStack state.");
         for (ViewFactory viewFactory : savedStack) {
             stack.push(viewFactory);
         }
@@ -63,6 +69,7 @@ public final class ViewStack {
     }
 
     public void saveToBundle(Bundle bundle) {
+        checkNotNull(bundle, "bundle == null");
         bundle.putSerializable(STACK_TAG, stack);
     }
 }
