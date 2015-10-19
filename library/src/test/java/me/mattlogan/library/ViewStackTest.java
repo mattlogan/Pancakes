ad638cf0daab5b4d213c58db9e21befce0350b7c
Matt Logan <matt.b.logan@gmail.com>
2015-10-19 12:00:31 -0600

    Remove @RunWith annotation in ViewStackTest

--- library/src/test/java/me/mattlogan/library/ViewStackTest.java
+++ library/src/test/java/me/mattlogan/library/ViewStackTest.java
@@ -9,11 +9,9 @@ import android.view.ViewTreeObserver;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.invocation.InvocationOnMock;
-import org.mockito.runners.MockitoJUnitRunner;
 import org.mockito.stubbing.Answer;
 
 import java.util.EmptyStackException;
@@ -34,7 +32,6 @@ import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 
-@RunWith(MockitoJUnitRunner.class)
 public class ViewStackTest {
 
     @Mock ViewStackDelegate delegate;
